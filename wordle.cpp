8167bc17b94dfafac1a16e2d914dbc1b948a0439
Michael/Misha Khmelkov <khmelkov@usc.edu>
2022-12-03 15:34:45 -0800

    Wordle implementation

--- wordle.cpp
+++ wordle.cpp
@@ -19,7 +19,49 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> results;
+    bool complete = true;
+    for (size_t i = 0; i<in.length(); i++) {
+        if (in[i] == '-') {
+            complete = false;
+            // Recurse for all possible letters
+            for (int letter = 97; letter<123; letter++) {
+                std::string temp = in;
+                temp[i] = letter;
+                std::set<std::string> returned = wordle(temp, floating, dict);
+                if (returned.size()>0) {
+                    for (std::set<std::string>::iterator it = returned.begin(); it != returned.end(); ++it) {
+                        results.insert(*it);
+                    }
+                }
+            }
+        }
+    }
+    if (complete) {
+        // If it's a real English word
+        if (dict.find(in) != dict.end()) {
+            // Check if all floating contained
+            bool containsAll = true;
+            for (size_t i = 0; i<floating.length(); i++) {
+                bool contains = false;
+                for (size_t j = 0; j<in.length(); j++) {
+                    if (in[j] == floating[i]) {
+                        contains = true;
+                        break;
+                    }
+                }
+                if (!contains) {
+                    containsAll = false;
+                    break;
+                }
+            }
+            // Add to results set if everything about word is satisfactory
+            if (containsAll) {
+                results.insert(in);
+            }
+        }
+    }
+    return results;
 }
 
 // Define any helper functions here
